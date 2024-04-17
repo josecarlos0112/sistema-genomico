fff8882b83b8fe0d3b58ef88c93cb73d1024489d
josecarlos0112 <jzorrgar@myuax.com>
2024-04-17 02:35:56 +0200

    modificacon del segundo modulo

--- src/main/java/analisis_numerico/CalculoPotencias.java
+++ src/main/java/analisis_numerico/CalculoPotencias.java
@@ -1,4 +1,11 @@
 package analisis_numerico;
 
 public class CalculoPotencias {
+    public static int potencia(int base, int exponente) {
+        int resultado = 1;
+        for (int i = 0; i < exponente; i++) {
+            resultado *= base;
+        }
+        return resultado;
+    }
 }
